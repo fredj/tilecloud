81c76b9e602771dee40784999bea71c60c08ec7a
fredj <frederic.junod@camptocamp.com>
2011-12-22 10:51:12 +0100

    Add MapnikTileStore and UTFGridTileStore (untested)

---
+++ tilecloud/store/mapnik.py
@@ -0,0 +1,42 @@
+import json
+import mapnik
+
+from tilecloud import Tile, TileStore
+
+
+
+class MapnikTileStore(TileStore):
+    def __init__(self, mapfile, **kwargs):
+        TileStore.__init__(self, **kwargs)
+        self.map = mapnik.Map(0, 0)
+        mapnik.load_map(self.map, mapfile)
+
+    def prepare_map(self, tile):
+        # fixme: tile size
+        tilesize = (256, 256)
+        # fixme: tile coord in geo (mapfile) unit
+        bounds = (-180.0, -90.0, 180.0, 90.0)
+
+        self.map.width, self.map.height = tilesize
+        self.map.zoom_to_box(mapnik.Box2d(*bounds))
+
+    def get_one(self, tile):
+        self.prepare_map(tile)
+        image = mapnik.Image(self.map.width, self.map.height)
+        mapnik.render(self.mapnik, image)
+        # fixme: content_type
+        tile.data = image.tostring()
+
+
+class UTFGridTileStore(MapnikTileStore):
+    def __init__(self, mapfile, layer_idx=0, fields=None, **kwargs):
+        MapnikTileStore.__init__(self, mapfile, **kwargs)
+        self.layer_idx = layer_idx
+        self.fields = fields
+
+    def get_one(self, tile):
+        self.prepare_map(tile)
+        grid = mapnik.Grid(self.map.width, self.map.height)
+        mapnik.render_layer(self.map, grid, layer=self.layer_idx, fields=self.fields)
+        # fixme: content_type
+        tile.data = json.encode(grid.encode())
